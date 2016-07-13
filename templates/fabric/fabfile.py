7bdb1ac87e4f7d23183bfb2acb315129b9adc713
jj <jj@jjdeMac.local>
2016-07-13 23:06:13 +0800

    update fabfile.py

--- templates/fabric/fabfile.py
+++ templates/fabric/fabfile.py
@@ -59,7 +59,7 @@ def package():
     excludes = ['tests', '.*', '*.log']
     local('rm -f dist/%s' % _TAR_FILE)
     with lcd(os.path.join(_current_path(), 'www')):
-        cmd = ['tar', '--dereference', '-czvf', '../dist/%s' % _TAR_FILE]
+        cmd = ['tar', '--dereference', '-czvf', 'dist/linux/%s' % _TAR_FILE]
         cmd.extend(['--exclude=\'%s\'' % ex for ex in excludes])
         cmd.extend(includes)
         local(' '.join(cmd))
