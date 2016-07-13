36e433dd2a301f08dbceeda35c486856532d83d5
jj <jj@jjdeMac.local>
2016-07-13 22:20:10 +0800

    add comand , basiclly use

--- main.go
+++ main.go
@@ -12,35 +12,95 @@ import (
 	"time"
 )
 
+var usage = `
+
+Usage : 
+1. megenerator <template> <name>				generate a text file"
+2. megenerator tpl init						first use to get templates
+3. megenerator tpl pull						update local templates
+4. megenerator tpl list						list local templates
+
+`
+
+var u *user.User
+var repPath string
+
+func init() {
+	u, _ = user.Current()
+	repPath = u.HomeDir + "/.megenerator"
+
+}
+
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage : megenerator <template-name> <name>")
+		fmt.Println(usage)
 		return
 	}
 
-	// .megenerator check
-	u, _ := user.Current()
-	repPath := u.HomeDir + "/.megenerator"
-	if os.Args[1] == "initpl" {
-		_, err := os.Stat(repPath)
-		if err != nil {
-			// git clone
-			exec.Command("git", "clone", "https://github.com/Jeremaihloo/megenerator").Run()
-			fmt.Println("git clone https://github.com/Jeremaihloo/megenerator")
-			exec.Command("mv", "megenerator", repPath).Run()
-			fmt.Printf("mv menegerator %s\n", u.HomeDir)
+	switch os.Args[1] {
+	case "tpl":
+		Init(os.Args[2])
+		break
+	default:
+		Generate(os.Args[1], os.Args[2])
+		break
+	}
+
+}
+
+func Init(opt string) {
+	switch opt {
+	case "init":
+		TplInit()
+		break
+	case "pull":
+		TplPull()
+		break
+	case "list":
+		TplList()
+		break
+	default:
+		fmt.Println(usage)
+		break
+	}
+}
+
+func TplInit() {
+	_, err := os.Stat(repPath)
+	if err != nil {
+		// git clone
+		cmd := exec.Command("git", "clone", "https://github.com/Jeremaihloo/megenerator")
+		cmd.Dir = u.HomeDir
+		if errCmd := cmd.Run(); errCmd != nil {
+			fmt.Println(errCmd.Error())
 		}
-		return
+	} else {
+		TplPull()
 	}
+}
 
+func TplPull() {
+	cmd := exec.Command("git", "pull")
+	cmd.Dir = repPath
+	cmd.Run()
+}
+
+func TplList() {
+	cmd := exec.Command("ls")
+	cmd.Dir = repPath + "/templates"
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func Generate(templateName, name string) {
 	meta := make(map[string]interface{})
 	meta["CreateAt"] = time.Now()
-	meta["Name"] = os.Args[1]
-	metaUri := repPath + "/templates/" + os.Args[1] + "/meta.json"
+	meta["Name"] = name
+	metaUri := repPath + "/templates/" + templateName + "/meta.json"
 	fmt.Println(metaUri)
 	jsonMetaData, _ := ioutil.ReadFile(metaUri)
 	json.Unmarshal(jsonMetaData, &meta)
-	tplSrc, _ := ioutil.ReadFile(repPath + "/templates/" + os.Args[1] + "/" + meta["Template"].(string))
+	tplSrc, _ := ioutil.ReadFile(repPath + "/templates/" + templateName + "/" + meta["Template"].(string))
 	t, err := template.New("template").Parse(string(tplSrc))
 	if err != nil {
 		fmt.Println(err.Error())
